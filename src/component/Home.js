972d0f92d4c8f0cd2ffd5b37b13f7028b9dadcbd
Branimir Maksimovic <maxava84@gmail.com>
2022-06-07 14:47:47 +0200

    context-reducer

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { CartState } from "../context/Context";
+import SingleProducts from "./SingleProducts";
 
 const Home = () => {
-  return <div>HOME</div>;
+  const {
+    state: { products },
+  } = CartState();
+  console.log(products);
+
+  return (
+    <div className="home">
+      {/* <Filters/> */}
+      <div className="productContainer">
+        {products.map((prod) => {
+          return <SingleProducts prod={prod} key={prod.id} />;
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
---
+++ src/context/Context.js
@@ -0,0 +1,32 @@
+import { faker } from "@faker-js/faker";
+import React, { useContext, useReducer } from "react";
+import { createContext } from "react";
+import { cartReducer } from "./Reducers";
+
+const Cart = createContext();
+
+const Context = ({ children }) => {
+  const products = [...Array(20)].map(() => ({
+    id: faker.datatype.uuid(),
+    name: faker.commerce.productName(),
+    price: faker.commerce.price(),
+    image: faker.image.technics(640, 480, true),
+    inStock: faker.helpers.arrayElement([2, 3, 4, 8, 9]),
+    fastDelivery: faker.datatype.boolean(),
+    raings: faker.helpers.arrayElement([1, 2, 3, 4, 5]),
+  }));
+
+  const [state, dispatch] = useReducer(cartReducer, {
+    products: products,
+    cart: [],
+  });
+
+  // console.log(products);
+  return <Cart.Provider value={{ state, dispatch }}>{children}</Cart.Provider>;
+};
+
+export default Context;
+
+export const CartState = () => {
+  return useContext(Cart);
+};
