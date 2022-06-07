705162137e30b7e7c684a4d1faef171fe78a7239
Branimir Maksimovic <maxava84@gmail.com>
2022-06-07 10:40:53 +0200

    icons-router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Cart from "./component/Cart";
 import Header from "./component/Header";
+import Home from "./component/Home";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </div>
     </div>
   );
 }
